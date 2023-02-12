6e834de2eaa21cf17edd2a1bcf5448d5e95cf276
Florent <mertens.florent00@gmail.com>
2023-02-12 23:03:23 +0100

    add js carousel

---
+++ main.js
@@ -0,0 +1,59 @@
+/* Carousel */
+
+var carousel = document.getElementById("carousel");
+var carouselBtn = document.getElementsByClassName("carouselBtn");
+
+var carouselImages = [
+  "url(/images/carousel.png)",
+  "url(/images/chef.png)",
+  "url(/images/ronaldo.webp)",
+];
+var currentCarouselImage = (carousel.style.backgroundImage = carouselImages[1]);
+
+window.addEventListener("load", () => {
+  carousel.style.backgroundImage = currentCarouselImage;
+});
+
+carouselBtn[0].addEventListener("click", () => {
+  var indexCarouselImage = carouselImages.findIndex(
+    (element) => element == currentCarouselImage
+  );
+  for (let index = 0; index <= carouselImages.length; index++) {
+    if (indexCarouselImage == index) {
+      if (indexCarouselImage <= 0) {
+        console.log("retour a la fin");
+        carousel.style.backgroundImage =
+          carouselImages[carouselImages.length - 1];
+        currentCarouselImage = carouselImages[carouselImages.length - 1];
+        break;
+      } else {
+        console.log("image precedente");
+        carousel.style.backgroundImage = carouselImages[indexCarouselImage - 1];
+        currentCarouselImage = carouselImages[indexCarouselImage - 1];
+        break;
+      }
+    }
+  }
+  console.log(currentCarouselImage);
+});
+
+carouselBtn[1].addEventListener("click", () => {
+  var indexCarouselImage = carouselImages.findIndex(
+    (element) => element == currentCarouselImage
+  );
+  for (let index = 0; index <= carouselImages.length; index++) {
+    if (indexCarouselImage == index) {
+      if (indexCarouselImage + 1 < carouselImages.length) {
+        console.log("prochaine image");
+        carousel.style.backgroundImage = carouselImages[indexCarouselImage + 1];
+        currentCarouselImage = carouselImages[indexCarouselImage + 1];
+        break;
+      } else {
+        console.log("retour a zero");
+        carousel.style.backgroundImage = carouselImages[0];
+        currentCarouselImage = carouselImages[0];
+        break;
+      }
+    }
+  }
+});
